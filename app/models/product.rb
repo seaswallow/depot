62179f455567101b419b788c04949275d7f25962
Sam Ruby <rubys@intertwingly.net>
2015-05-01 03:57:02 -0700

    Check out

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ActiveRecord::Base
 	validates :title, length: { minimum: 10 }, allow_blank: true
 	
 	has_many :line_items
+	has_many :orders, through: :line_items
 
 	before_destroy :ensure_not_referenced_by_any_line_item
 
