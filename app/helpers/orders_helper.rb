79495cf0f43cb381151efef2bc76db3fdab198c2
swallow <swallow321@126.com>
2015-05-03 00:01:37 +0800

    Internationalization

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,9 @@
 module OrdersHelper
+  def payment_type_options
+    options_for_select(
+    	Order::PAYMENT_TYPES.map { |x| 
+    		[ I18n.t('orders.payment_types.' + x.parameterize.underscore), x ] 
+    	}  
+    )    
+  end
 end
